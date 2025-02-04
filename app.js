edff1ba6faccf0f00e3e0d7f8bdd49ff122cedb8
PaterDev <josepater_1130@hotmail.com>
2025-02-04 18:36:43 -0500

    feat: added friends and the list is updated in the DOM

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+// Función para agregar amigos
+function agregarAmigo() {
+  let elementoCampo = document.getElementById("amigo");
+  let campoAmigo = elementoCampo.value;
+
+  // Validar la entreda de amigos
+  if (isNaN(campoAmigo)) {
+    amigos.push(campoAmigo); // Agregar amigo al array
+    actualizarListaAmigos(); // Actualizar DOM
+    elementoCampo.value = ""; // Limpiar campo
+  } else {
+    alert("Campo vacío");
+  }
+}
+
+// Actualizar lista de amigos en el DOM
+function actualizarListaAmigos() {
+  const elementoListaAmigos = document.getElementById("listaAmigos");
+
+  elementoListaAmigos.innerHTML = ""; // Limpiar el elemento de la lista de amigos
+
+  // Agregar ítems
+  amigos.forEach(function (amigo) {
+    const li = document.createElement("li"); // Crear un nuevo ítem
+    li.textContent = amigo; // Asignar el nombre en el ítem
+    elementoListaAmigos.appendChild(li); // Agregar el <li> al <ul>
+  });
+}
