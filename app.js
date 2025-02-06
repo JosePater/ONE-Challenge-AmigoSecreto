815d5a6b13c93c16648edc8a4f254b5d8cc115aa
PaterDev <josepater_1130@hotmail.com>
2025-02-06 15:03:34 -0500

    feat: added function to choose a friend when grilling with validation

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 let amigos = [];
 
 // Función para agregar amigos
-function agregarAmigo() {
+function agregarAmigo(event) {
+  event.preventDefault(); // Evitar recargar la página
   let elementoCampo = document.getElementById("amigo");
   let campoAmigo = elementoCampo.value;
 
   // Validar la entreda de amigos
   if (isNaN(campoAmigo)) {
-    amigos.push(campoAmigo); // Agregar amigo al array
-    actualizarListaAmigos(); // Actualizar DOM
-    elementoCampo.value = ""; // Limpiar campo
+    // Validar nombre repetidos
+    if (amigos.includes(campoAmigo)) {
+      alert(`El nombre '${campoAmigo}' ya existe!`);
+    } else {
+      amigos.push(campoAmigo); // Agregar amigo al array
+      actualizarListaAmigos(); // Actualizar DOM
+      elementoCampo.value = ""; // Limpiar campo
+    }
   } else {
     alert("Campo vacío");
   }
@@ -27,4 +33,27 @@ function actualizarListaAmigos() {
     li.textContent = amigo; // Asignar el nombre en el ítem
     elementoListaAmigos.appendChild(li); // Agregar el <li> al <ul>
   });
+}
+
+// Sortear amigo
+function sortearAmigo() {
+  const cantAmigos = amigos.length; // Cantidad de amigos
+
+  // Validar cantidad de amigos suficiente para sorteo
+  if (cantAmigos < 2) {
+    alert("Cantidad de amigos insuficiente para el sorteo");
+  } else {
+    const indexAmigo = Math.floor(Math.random() * cantAmigos); // Índice del amigo sorteado
+    const amigoSorteado = amigos[indexAmigo]; // Amigo sorteado
+
+    const elementoAmigoSorteado = document.getElementById("resultado"); // Elemento del amigo sorteado
+    const li = document.createElement("li"); // Crear un nuevo ítem
+
+    // Borrar todos los hijos del elemento
+    while (elementoAmigoSorteado.firstChild) {
+      elementoAmigoSorteado.removeChild(elementoAmigoSorteado.firstChild); // Borrar todos los hijos del elemento
+    }
+    li.textContent = amigoSorteado; // Asignar el nombre en el ítem
+    elementoAmigoSorteado.appendChild(li); // Agregar el <li> (amigo soretado) al <ul>
+  }
 }
